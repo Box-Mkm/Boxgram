03124d8ffda6d6084c0802abb0c78549ca4f1f63
Box-Mkm <mohamedkhaledx12@gmail.com>
2023-04-18 07:18:37 +0200

    03

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,12 +13,14 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('username');
+            $table->string(column:'name');
+            $table->string(column:'username');
+            $table->text(column:'bio')->nullable();
             $table->string(column:'image');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->boolean(column: 'private_account')->default(0);
+            $table->string(column:'email')->unique();
+            $table->timestamp(column:'email_verified_at')->nullable();
+            $table->string(column:'password');
             $table->rememberToken();
             $table->timestamps();
         });
---
+++ database/migrations/2023_04_17_042456_create_posts_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string(column: 'image');
+            $table->text(column:'description');
+            $table->text(column: 'slug');
+            $table->integer(column:'likes')->default(0);
+            $table->foreignId(column:'user_id')->constrained()->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
