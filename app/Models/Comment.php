8b48adea1794459a4532402802f020d5081fc20a
Box-Mkm <mohamedkhaledx12@gmail.com>
2023-04-26 14:04:48 +0200

    04- Comments w keda

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -12,6 +12,7 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 class Comment extends Model
 {
     use HasFactory;
+    protected $fillable= ['user_id','body'];
     public function post():BelongsTo
     {
         return $this->belongsto(Post::class);
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CommentController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
@@ -25,6 +26,8 @@ Route::get('/dashboard', function () {
 
 Route::get('/p/create',[PostController::class , 'create'])->name('created_post')->middleware(middleware:'auth');
 Route::post('/p/create',[PostController::class , 'store'])->name('store_post')->middleware(middleware:'auth');
+Route::get('/p/{post:slug}',[PostController::class , 'show'])->name('show_post')->middleware(middleware:'auth');
+Route::post('/p/{post:slug}/comment',[CommentController::class , 'store'])->name('store_comment')->middleware(middleware:'auth');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
